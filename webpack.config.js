e19ff4754dbcde3e06b22b2896c79288da979d17
Fanchik314G <stem4ikwar@gmail.com>
2026-09-14 19:59:09 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,22 @@
+const path = require('path');
+const JavaScriptObfuscator = require('webpack-obfuscator');
 
+module.exports = {
+  mode: 'production',
+  entry: {
+    server: './index.js',
+    app: './app.js'
+  },
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new JavaScriptObfuscator({
+      rotateStringArray: true,
+      stringArray: true,
+      compact: true
+    }, ['server.js'])
+  ]
+};
